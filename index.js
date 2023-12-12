2d17bbe391b5efe0a555f8ed576176e47c943545
everttoncampos <evertton.g.l@hotmail.com>
2023-12-12 10:16:25 -0400

    Deixando apenas console log no arquivo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const GoogleSpreadsheet = require('google-spreadsheet');
-const credentials = require('./credentials.json');
-const JWT = require('google-auth-library');
+// const GoogleSpreadsheet = require('google-spreadsheet');
+// const credentials = require('./credentials.json');
+// const JWT = require('google-auth-library');
 
-const serviceAccountAuth = new JWT.JWT ({
-    email:credentials.client_email,
-    key:credentials.private_key,
-    scopes: [
-        'https://www.googleapis.com/auth/spreadsheets',
-    ]
-});
+// const serviceAccountAuth = new JWT.JWT ({
+//     email:credentials.client_email,
+//     key:credentials.private_key,
+//     scopes: [
+//         'https://www.googleapis.com/auth/spreadsheets',
+//     ]
+// });
 
-// const docIdteste = "1J4IeLYGcgCAaPM7KHBCYjPO23tnLM9A5r5L3KNUtDWk"
-const docIdteste = "1FKDu53W5dQD99nvv6-b7ldGr-U7ZWXOo_rQDRirLcMk";
-const docTeste = new GoogleSpreadsheet.GoogleSpreadsheet(docIdteste, serviceAccountAuth);
+// // const docIdteste = "1J4IeLYGcgCAaPM7KHBCYjPO23tnLM9A5r5L3KNUtDWk"
+// const docIdteste = "1FKDu53W5dQD99nvv6-b7ldGr-U7ZWXOo_rQDRirLcMk";
+// const docTeste = new GoogleSpreadsheet.GoogleSpreadsheet(docIdteste, serviceAccountAuth);
 
 // const loadSheetMes = async function (){
 //     await docTeste.loadInfo();
