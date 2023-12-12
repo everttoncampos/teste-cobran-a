525eb392a64f20b9beab141af670b2a849c29fbd
everttoncampos <evertton.g.l@hotmail.com>
2023-12-12 10:06:54 -0400

    Testeando forma de funcionar o codigo no servidor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const GoogleSpreadsheet = require('google-spreadsheet')
-const credentials = require('./credentials.json')
-const JWT = require('google-auth-library')
+const GoogleSpreadsheet = require('google-spreadsheet');
+const credentials = require('./credentials.json');
+const JWT = require('google-auth-library');
 
 const serviceAccountAuth = new JWT.JWT ({
     email:credentials.client_email,
@@ -8,17 +8,17 @@ const serviceAccountAuth = new JWT.JWT ({
     scopes: [
         'https://www.googleapis.com/auth/spreadsheets',
     ]
-})
+});
 
 // const docIdteste = "1J4IeLYGcgCAaPM7KHBCYjPO23tnLM9A5r5L3KNUtDWk"
-const docIdteste = "1FKDu53W5dQD99nvv6-b7ldGr-U7ZWXOo_rQDRirLcMk"
-const docTeste = new GoogleSpreadsheet.GoogleSpreadsheet(docIdteste, serviceAccountAuth)
+const docIdteste = "1FKDu53W5dQD99nvv6-b7ldGr-U7ZWXOo_rQDRirLcMk";
+const docTeste = new GoogleSpreadsheet.GoogleSpreadsheet(docIdteste, serviceAccountAuth);
 
-const loadSheetMes = async function (i){
-    await docTeste.loadInfo()
+const loadSheetMes = async function (){
+    await docTeste.loadInfo();
 
-    const sheet = await docTeste.sheetsByIndex[i]
-    const rows = await sheet.getRows()
+    const sheet = await docTeste.sheetsByIndex[0];
+    const rows = await sheet.getRows();
 
     // let dados = []
 
@@ -34,41 +34,43 @@ const loadSheetMes = async function (i){
             Parcela: item.get('N Parcela')
         }
 
-    })
+    });
 
     // let dia = dados[1].Data.split('/')[0]
     // let mes = dados[1].Data.split('/')[1]
     // let ano = dados[1].Data.split('/')[2]
     // console.log(`O dia de vencimento desse cliente é: ${dia}, do mês ${mes}, do ano ${ano}`)
-    console.log(dados)
+    console.log(dados);
 
-    return dados
+    return dados;
 }
 
-const loadSheetFevereiro = async function (){
+// const loadSheetFevereiro = async function (){
 
-    await docTeste.loadInfo()
+//     await docTeste.loadInfo()
 
-   const sheet = await docTeste.sheetsByIndex[1]
-   const rows = await sheet.getRows()
+//    const sheet = await docTeste.sheetsByIndex[1]
+//    const rows = await sheet.getRows()
 
-   let dados = []
+//    let dados = []
 
-   rows.map(item => {
+//    rows.map(item => {
 
-       dados.push({
-           'Nome': item.get('Nome'),
-           'Telefone': item.get('Telefone'),
-           'Valor': item.get('Valor'),
-           'Status': item.get('Status')
-       })
+//        dados.push({
+//            'Nome': item.get('Nome'),
+//            'Telefone': item.get('Telefone'),
+//            'Valor': item.get('Valor'),
+//            'Status': item.get('Status')
+//        })
 
-   })
+//    })
 
 //    console.log(dados)
 
 //    return dados
-}
+// }
+
+loadSheetMes()
 
 dadosClientes = {
     loadSheetMes,
